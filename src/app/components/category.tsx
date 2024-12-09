b1d132da9a5f4f12550b72577ac8613a9338b494
Asad Ali <theeasadali@gmail.com>
2024-12-10 01:42:47 +0500

    Homepage without category plus

--- src/app/components/category.tsx
+++ src/app/components/category.tsx
@@ -11,7 +11,7 @@ const Category:React.FC<Category> = ({name, image, noOfItems}) => {
   return (
     <div className='rounder-[10px] relative max-w-[400px]'>
         <Image src={image} height={424} width={424} alt={name}/>
-        <div className='flex flex-col gap-2 absolute bottom-0 left-0 bg-overlary p-5 rounded-b-[10px]'>
+        <div className='flex flex-col gap-2 absolute bottom-0 left-0 bg-overlary p-5 rounded-b-[10px] w-full'>
             <span className='font-semibold text-xl text-white'>{name}</span>
             <span className='text-sm text-white opacity-70'>{`${noOfItems}Products`}</span>
         </div>
